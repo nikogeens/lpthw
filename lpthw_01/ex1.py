2fe8b207b1f77178f7beb1872510da281d9219c6
Niko Geens <niko.geens@mia.be>
2013-02-15 14:51:41 +0100

    lpthw - lesson01 completed

---
+++ lpthw_01/ex1.py
@@ -0,0 +1,9 @@
+# print "Hello World!"
+# print "Hello again"
+# print "I like typing this."
+print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not thouch this.'
+# print "---"
+# print "This is another line."
