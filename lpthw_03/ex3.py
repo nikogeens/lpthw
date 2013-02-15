ca7a15ae813555681b8c1f841930972ddc166716
Niko Geens <niko.geens@mia.be>
2013-02-15 15:33:37 +0100

    lpthw - lesson 03 completed

---
+++ lpthw_03/ex3.py
@@ -0,0 +1,32 @@
+print "I will now count my chickens:"
+
+# Math operations don't have to be in a string
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Comparison return's a true or false
+print "Is it true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it is false."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# How many eggs per hen?
+print "That's a ratio of approximately ", (25 + 30 / 6) / (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 * 6) * 1.00, "per hen..."
+
+# The importance of rounding
+print 7 / 4
+print 7.0 / 4.0
+# the number of decimals seems not to be important, just the fact htat it's decimal
+print 7.0000 / 4
