5557d9169faa5596086ee047b04dc2fba8690b89
Niko Geens <niko.geens@gmail.com>
2013-02-17 21:03:35 +0100

    lpthw - lesson  09 completed

---
+++ lpthw_09/ex09.py
@@ -0,0 +1,18 @@
+# Here's some new strange stuff, I'll try to type it exactly.
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+print "Here are the months: %r " % months
+print "Here are the months: %s " % months
+# print "Here are the months: %d " % months
+
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+
